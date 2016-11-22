00c94ad446b704e7bdf249c8af5605f6b2ba739e
Chris Warburton <chriswarbo@gmail.com>
2016-11-22 19:29:46 +0000

    Bump

--- helpers/runscript.sh
+++ helpers/runscript.sh
@@ -2,10 +2,11 @@
 BASE=$(dirname "$(readlink -f "$0")")
 
 echo "Looking up derivation for '$1'" 1>&2
+# shellcheck disable=SC2016
 DRV=$("$BASE"/tests.sh | jq -r --arg name "$1" '.[$name].drv')
 
 echo "Realising '$DRV'" 1>&2
-SCRIPT=$(nix-store -r "$DRV")
+SCRIPT=$(nix-store --show-trace -r "$DRV")
 
 echo "Running '$SCRIPT'" 1>&2
 "$SCRIPT"
