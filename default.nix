b0aa9b18540d7340320bf50bae6e0afa7823b259
Chris Warburton <chriswarbo@gmail.com>
2017-01-29 11:02:56 +0000

    Simplify

--- default.nix
+++ default.nix
@@ -1,31 +1,29 @@
 # Many of our tests use custom Nix overrides, so we fetch those first.
 with rec {
-  # We need some version of nixpkgs in order to fetch git repos; use default
+  # Loads nixpkgs with a particular config
+  pkgsWith = config: import <nixpkgs> {
+    config = import "${config}/config.nix";
+  };
+
+  # Use the default nixpkgs to fetch git repos
   pristinePkgs = import <nixpkgs> {};
 
-  # Nixpkgs only lets us fetch a known git commit, so these overrides are old
-  configUrl    = http://chriswarbo.net/git/nix-config.git;
-  knownConfig  = pristinePkgs.fetchgit {
+  # We're limited to pre-specified git commits, so load one of our overrides
+  configUrl         = http://chriswarbo.net/git/nix-config.git;
+  oldOverriddenPkgs = pkgsWith (pristinePkgs.fetchgit {
     url    = configUrl;
     rev    = "2a83e1bb0fcfd0dc293221812fbf4a03ed37f9f0";
     sha256 = "1jdsh7ysq6z1hx9z56qiypcazy8b0nhhgcykmxdfm4yy0lirspw2";
-  };
-  oldOverriddenPkgs = import <nixpkgs> {
-                        config = import "${knownConfig}/config.nix";
-                      };
+  });
 
   # Use the old overrides to get the latest version of the overrides
-  config  = oldOverriddenPkgs.latestGit { url = configUrl; };
-  newPkgs = import <nixpkgs> {
-              config = import "${config}/config.nix";
-            };
+  newPkgs = pkgsWith (oldOverriddenPkgs.latestGit { url = configUrl; });
 };
 with newPkgs;
 with lib;
 
 stdenv.mkDerivation {
   name         = "tests";
-  src          = ./.;
   buildInputs  = map (t: t.test) (import ./tests.nix { pkgs = newPkgs; });
   buildCommand = ''
     echo "Passed" > "$out"
