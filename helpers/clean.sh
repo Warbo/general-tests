c45c786e3aee14fc96ca7a6e728a11fefb356ed9
Chris Warburton <chriswarbo@gmail.com>
2017-06-15 15:30:47 +0100

    Update clean.sh

--- helpers/clean.sh
+++ helpers/clean.sh
@@ -1,2 +1,25 @@
 #!/usr/bin/env bash
-./run clean
+
+set -e
+
+cd "$(dirname "$(readlink -f "$0")")"
+
+PTHS=$(./refreshAttrs.sh)
+
+for F in ../results/pass/* ../results/fail/*
+do
+    FOUND=0
+    NAME=$(basename "$F")
+    while read -r PTH
+    do
+        if [[ "x$NAME" = "x$PTH" ]]
+        then
+            FOUND=1
+        fi
+    done < <(echo "$PTHS")
+
+    if [[ "$FOUND" -eq 0 ]]
+    then
+        rm -v "$F"
+    fi
+done
