8fe2d80ac6c286e5747e2849816caa8f3051c39a
Chris Warburton <chriswarbo@gmail.com>
2017-06-19 12:46:41 +0100

    Run Emacs tests

--- tests/emacs.nix
+++ tests/emacs.nix
@@ -1,8 +1,14 @@
 { helpers, pkgs }:
 with pkgs;
-runCommand "dummy" {} "exit 1"
-
-/*
-#!/usr/bin/env bash
-~/.emacs.d/tests/run.sh
-*/
+runCommand "emacs"
+  {
+    CFG         = latestGit { url = helpers.repoOf "warbo-emacs-d"; };
+    buildInputs = [ emacs ];
+  }
+  ''
+    export HOME="$PWD/home"
+    mkdir "$HOME"
+    cp -r "$CFG" "$HOME/.emacs.d"
+    chmod +w -R "$HOME/.emacs.d"
+    "$HOME/.emacs.d/test-runner.sh"
+  ''
