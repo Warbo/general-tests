adad81dfc68d881d35c4f9e34852e32149788a06
Chris Warburton <chriswarbo@gmail.com>
2017-07-05 17:05:53 +0100

    Convert shellcheck-tests to Nix
    It runs, but may be giving spurious passes. We could also do better by splitting
    it up to check different git repos, as well as stuff from $HOME

--- tests/shellcheck-tests.nix
+++ tests/shellcheck-tests.nix
@@ -1,68 +1,61 @@
 { helpers, pkgs }:
 with pkgs;
-runCommand "dummy" {} "exit 1"
+with rec {
+  REAL_HOME = "/home/chris";
+};
+runCommand "dummy"
+  {
+    inherit REAL_HOME;
+    buildInputs = [ shellcheck warbo-utilities ];
+  }
+  ''
+    #!/usr/bin/env bash
 
-/*
-#! /usr/bin/env bash
+    shopt -s nullglob
 
-shopt -s nullglob
+    if [[ -e "$REAL_HOME" ]]
+    then
+      export HOME="$REAL_HOME"
+    fi
 
-# Pass in the argument "full" to keep going after a failure
-FULL=0
-[[ "x$1" = "xfull" ]] && FULL=1
-
-function msg {
-    true
-    echo "$1" 1>&2
-}
-
-function data {
-    my_shellscripts | skip
-}
-
-function rawCached {
-    ./helpers/cache.sh "$(basename "$0")" < <(data)
-}
-
-function skip {
-    grep -v "\.rkt$"                |
-    grep -v "/haskell-te/packages/" |
-    grep -v "/TheoryExplorationBenchmark/modules/" | while read -r F
-    do
+    function skip {
+      grep -v "\.rkt$"                |
+      grep -v "/haskell-te/packages/" |
+      grep -v "/TheoryExplorationBenchmark/modules/" | while read -r F
+      do
         if head -n1 < "$F" | grep    "nix-shell"     > /dev/null &&
            head -n2 < "$F" | grep -- "-i runhaskell" > /dev/null
         then
-            # Skip Haskell file
-            continue
+          # Skip Haskell file
+          continue
         fi
         if head -n1 < "$F" | grep "racket" > /dev/null
         then
-            # Skip Racket file
-            continue
+          # Skip Racket file
+          continue
         fi
         echo "$F"
-    done
-}
-
-function cached {
-    # Some files may have disappeared since we last cached
-    while read -r LINE
-    do
-        [[ -f "$LINE" ]] && echo "$LINE"
-    done < <(rawCached | skip)
-}
-
-cached > /dev/null
-
-ERR=0
-while read -r script
-do
-    printf '.'
-    shellcheck -e SC1091 -e SC1008 -e SC2001 -e SC2029 "$script" || {
-        ERR=1
-        [[ "$FULL" -eq 1 ]] || exit 1
+      done
     }
-done < <(cached)
 
-exit "$ERR"
-*/
+    ERR=0
+    while read -r script
+    do
+      [[ -f "$script" ]] || continue
+      echo "Checking '$script'" 1>&2
+      if shellcheck -e SC1091 -e SC1008 -e SC2001 -e SC2029 "$script"
+      then
+        echo "Passed: '$script'" 1>&2
+      else
+        echo "Failed: '$script'" 1>&2
+        ERR=1
+      fi
+    done < <(my_shellscripts | skip)
+
+    if [[ "$ERR" -eq 0 ]]
+    then
+      echo "pass" > "$out"
+    fi
+
+    exit "$ERR"
+  ''
