c612d30fdb106ccd9ccdad5d72a4dfa0f762f9eb
Chris Warburton <chriswarbo@gmail.com>
2017-09-15 02:46:32 +0100

    Just use existing Haskell library functions

--- tests/cabal-test.nix
+++ tests/cabal-test.nix
@@ -1,25 +1,10 @@
 { pkgs ? import <nixpkgs> {}, helpers ? {} }:
 with builtins;
 with rec {
-  inherit (pkgs)
-    bash gnused haskellPackages jq lib runCommand sanitiseName stdenv;
-
-  inherit (lib)
-    mapAttrs;
-
-  inherit (helpers)
-    compileHaskell haskellRepos myHaskell repoOf;
-
-  testRepo = name: repo:
-    stdenv.mkDerivation {
-      name         = "cabal-test";
-      buildInputs  = [ (compileHaskell name repo "test") ];
-      buildCommand = ''
-        echo "Passed" > "$out"
-      '';
-    };
-
-  tests = mapAttrs testRepo myHaskell;
+  inherit (pkgs)    haskell lib;
+  inherit (lib)     mapAttrs;
+  inherit (helpers) haskellTinced myHaskell;
 };
 
-tests
+mapAttrs (_: repo: haskell.lib.doCheck (haskellTinced repo))
+         myHaskell
