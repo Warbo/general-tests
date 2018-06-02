05ca0fbcc5e2ef779ccd79e0c14607213f5ef3ea
Chris Warburton <chriswarbo@gmail.com>
2018-06-02 11:26:27 +0100

    Reenable STP test

--- tests/STP-2015-10.nix
+++ tests/STP-2015-10.nix
@@ -1,15 +1,13 @@
 { helpers, pkgs }:
-helpers.notImplemented "stp-2015-10"/*
 with pkgs;
-
-runCommand "have-STP"
-  {
-    buildInputs = [ fail ];
-    dir         = helpers.inputFallback "writing";
-  }
-  ''
+wrap {
+  name  = "have-STP";
+  paths = [ bash fail ];
+  vars  = { dir = helpers.inputFallback "writing"; };
+  script = ''
+    #!/usr/bin/env bash
+    set -e
     [[ -e "$dir/STP/sites/stp/2015/10/index.md" ]] || fail "Have no site"
     [[ -e "$dir/STP/talks/2015/10/slides.md"    ]] || fail "Have no slides"
-    echo pass > "$out"
-  ''
-*/
+  '';
+}
