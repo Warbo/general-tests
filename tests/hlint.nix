7359676fed9064f25c8ca7b571f6d293e5f2e11c
Chris Warburton <chriswarbo@gmail.com>
2018-06-02 11:24:01 +0100

    Make hlint tests work again
    We also do this in a two stage process: we use runCommand to find Haskell repos,
    import the result and generate a test script for each. This way we can run those
    test scripts over and over without having to lookup repos again.
    
    To force the repos to be looked up, we just rebuild the test command.

--- tests/hlint.nix
+++ tests/hlint.nix
@@ -1,14 +1,9 @@
 { pkgs ? import <nixpkgs> {}, helpers ? {} }:
-helpers.notImplemented "hlint"/*
+
 with builtins;
-with {
-
-};
-
 with rec {
   inherit (pkgs)
-    bash findutils gnused haskellPackages jq lib runCommand sanitiseName
-    stdenv;
+    bash haskellPackages lib wrap;
 
   inherit (lib)
     mapAttrs;
@@ -16,60 +11,16 @@ with rec {
   inherit (helpers)
     myHaskell;
 
-getProjects = stdenv.mkDerivation {
-  name         = "projects";
-  buildInputs  = [ findutils gnused jq ];
-  buildCommand = ''
-    shopt -s nullglob
-    echo "[" >> "$out"
-
-    if [[ -d /home/chris/Programming ]]
-    then
-      # Standalone Haskell files
-      #DIR="/home/chris/Programming/Haskell/"
-      #echo "$DIR"*.hs
-      #echo "$DIR"*.lhs
-      true
-      # Project directories
-      #LOCATE_PATH=/var/cache/locatedb "$../helpers/my_haskell.sh"    |
-      #  grep -v "/Haskell/quickcheck$" |
-      #  grep -v "/Haskell/imm$"        |
-      #  grep -v "/Haskell/ifcxt$"
-    else
-      echo "No projects on disk" 1>&2
-    fi
-    echo "]" >> "$out"
-  '';
-};
-
-projects = import "${getProjects}";
-
-testCommand = ''
-  hlint -XNoCPP "--ignore=Parse error" "$src" && echo "Passed" > "$out"
-'';
-
-mkTest = src: stdenv.mkDerivation {
-  inherit src;
-  name         = "hlint-test";
-  buildInputs  = [ haskellPackages.hlint ];
-  buildCommand = testCommand;
-};
-
-testRepo = src:
-  stdenv.mkDerivation {
-    inherit src;
-    name         = "test-repo";
-    buildInputs  = [ haskellPackages.hlint ];
-    buildCommand = testCommand;
+  testRepo = name: src: wrap {
+    name   = "hlint-${name}";
+    paths  = [ bash haskellPackages.hlint ];
+    vars   = { inherit src; };
+    script = ''
+      #!/usr/bin/env bash
+      set -e
+      hlint -XNoCPP "--ignore=Parse error" "$src"
+    '';
   };
-
-tests = (mapAttrs (_: testRepo) myHaskell) //
-        (listToAttrs (map (name: {
-                            inherit name;
-                            value = mkTest name;
-                          })
-                          projects));
 };
 
-tests
-*/
+mapAttrs testRepo myHaskell
