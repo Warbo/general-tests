f7bc59402a74cfa193c2a3095d2964f24f2b7533
Chris Warburton <chriswarbo@gmail.com>
2018-06-02 11:23:13 +0100

    Simplify getting nix-config

--- helpers/nix-config.nix
+++ helpers/nix-config.nix
@@ -2,20 +2,20 @@
 
 with builtins;
 with rec {
-  bootPkgs   = config: import <nixpkgs> { inherit config; };
-  bootConfig = (bootPkgs {}).fetchgit {
+  pkgs       = if nixpkgs == null then <nixpkgs> else nixpkgs;
+  local      = /home/chris/nix-config;
+  remote     = (import pkgs { config = {}; }).fetchgit {
     url    = http://chriswarbo.net/git/nix-config.git;
     rev    = "d1b2b9b";
     sha256 = "1rsax2izq5083wlxssg0ch4bxkg2g1hm2v61vp8frg5v9q55rlgr";
   };
+  config-src = if nix-config != null
+                  then nix-config
+                  else if pathExists local
+                          then local
+                          else (import remote {
+                                 unstablePath = pkgs;
+                               }).latestNixCfg;
 };
 
-import (if nixpkgs == null then <nixpkgs> else nixpkgs) {
-  config = if nix-config == null
-              then with tryEval <nix-config>;
-                   if success
-                      then import "${value}/unstable.nix"
-                      else with bootPkgs (import "${bootConfig}/config.nix");
-                           latestNixCfg
-              else import "${nix-config}/unstable.nix";
-}
+import config-src { unstablePath = pkgs; }
