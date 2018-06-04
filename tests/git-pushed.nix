887d5ff64d3ddaa8cd3b33c7514998c238a3dc7b
Chris Warburton <chriswarbo@gmail.com>
2018-06-04 11:04:36 +0100

    Port git-pushed to new implementation

--- tests/git-pushed.nix
+++ tests/git-pushed.nix
@@ -1,115 +1,78 @@
 { helpers, pkgs }:
-helpers.notImplemented "git-pushed"
 
-/*
-#!/usr/bin/env bash
+with builtins;
+with pkgs;
+with lib;
+with {
+  check = name: repo: wrap {
+    name   = "git-pushed-${name}";
+    paths  = [ bash fail git ];
+    vars   = { inherit repo; };
+    script = ''
+      #!/usr/bin/env bash
+      set -e
 
-# Pass in the argument "full" to keep going after a failure
-FULL=0
-[[ "x$1" = "xfull" ]] && FULL=1
+      function remotes {
+          # We should always be pushing to origin and github; we don't enforce
+          # anything else since they're case-by-case. For example, we might
+          # have a read-only "upstream" for fetching, with pushes going via
+          # pull requests from "github"
+          git remote | grep -e '\(origin\|github\)'
+      }
 
-function fail {
-    msg "FAIL: $1"
-    ERR=1
-    [[ "$FULL" -eq 1 ]] || exit 1
-}
+      function branches {
+          git branch | cut -c 3- | grep -v '^('
+      }
 
-function repos {
-    # Find git repos based on their .git directories
-    locate -e "/home/chris/ * /.git" | skip
-}
+      function checkBranch {
+          git ls-remote "$1" 2> /dev/null | cut -f 2 |
+                                            grep -Fx "refs/heads/$2" ||
+              fail "'$repo' branch '$BRANCH' not on remote '$1'"
+      }
 
-function msg {
-    echo -e "$1" 1>&2
-}
+      function skip {
+          grep -v "opencl-horde"          |
+          grep -v "/Backups/"             |
+          grep -v "/git-html"             |
+          grep -v "/System/Programs"      |
+          grep -v "/NotMine/"             |
+          grep -v "/OldCode/"             |
+          grep -v "/Marking/"             |
+          grep -v "/haskell-te/packages/" |
+          grep -v "\.stack"               |
+          grep -v "/TheoryExplorationBenchmark/modules/"
+      }
 
-function remotes {
-    # We should always be pushing to origin and github; we don't enforce
-    # anything else since they're case-by-case. For example, we might
-    # have a read-only "upstream" for fetching, with pushes going via
-    # pull requests from "github"
-    git remote | grep -e '\(origin\|github\)'
-}
+      function checkRemotes {
+          # Look for unpushed commits and missing branches. `git status` also lists
+          # unpushed commits but it doesn't work in bare repos
+          while read -r REMOTE
+          do
+              checkBranches "$REMOTE"
+          done < <(remotes)
+      }
 
-function branches {
-    git branch | cut -c 3- | grep -v '^('
-}
+      function checkBranches {
+          MISSINGBRANCHES=0
+          while read -r BRANCH
+          do
+              checkBranch "$1" "$BRANCH" || MISSINGBRANCHES=1
+          done < <(branches)
+          return "$MISSINGBRANCHES"
+      }
 
-function checkBranch {
-    git ls-remote "$1" 2> /dev/null | cut -f 2 |
-                                      grep -Fx "refs/heads/$2" > /dev/null || {
-        fail "'$REPO' branch '$BRANCH' not on remote '$1'"
-        return 1
-    }
-}
+      [[ -e "$repo" ]] || { echo "Skipping non-existent '$repo'"; continue; }
+      echo "Checking '$repo'" 1>&2
 
-function data {
-    while read -r GITDIR
-    do
-        dirname "$GITDIR"
-    done < <(repos)
+      pushd "$repo" > /dev/null
 
-    # Include all our bare repos
-    ls -d1 /home/chris/Programming/repos/*.git
-}
+      # Skip repos with no remote
+      [[ -z "$(git remote)" ]] && exit 0
 
-function skip {
-    grep -v "opencl-horde"          |
-    grep -v "/Backups/"             |
-    grep -v "/git-html"             |
-    grep -v "/System/Programs"      |
-    grep -v "/NotMine/"             |
-    grep -v "/OldCode/"             |
-    grep -v "/Marking/"             |
-    grep -v "/haskell-te/packages/" |
-    grep -v "\.stack"               |
-    grep -v "/TheoryExplorationBenchmark/modules/"
-}
+      checkRemotes
 
-function cached {
-    ./helpers/cache.sh "git-pushed" < <(data | skip) | skip
-}
-
-cached > /dev/null
-
-ERR=0
-
-function checkRepos {
-    while read -r REPO
-    do
-        [[ -e "$REPO" ]] || { echo "Skipping non-existent '$REPO'"; continue; }
-        echo "Checking '$REPO'" 1>&2
-
-        pushd "$REPO" > /dev/null
-
-        # Skip repos with no remote
-        [[ -z "$(git remote)" ]] && continue
-
-        checkRemotes
-
-        popd > /dev/null
-    done < <(cached)
-}
-
-function checkRemotes {
-    # Look for unpushed commits and missing branches. `git status` also lists
-    # unpushed commits but it doesn't work in bare repos
-    while read -r REMOTE
-    do
-        checkBranches "$REMOTE"
-    done < <(remotes)
-}
-
-function checkBranches {
-    MISSINGBRANCHES=0
-    while read -r BRANCH
-    do
-        checkBranch "$1" "$BRANCH" || MISSINGBRANCHES=1
-    done < <(branches)
-    return "$MISSINGBRANCHES"
-}
-
-checkRepos
-
-exit "$ERR"
-*/
+      popd > /dev/null
+    '';
+  };
+};
+mapAttrs check (helpers.localRepos // helpers.myRepos)
