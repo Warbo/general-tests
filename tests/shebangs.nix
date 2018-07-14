97369308cfd96471d029d5316fa3206f62851b3b
Chris Warburton <chriswarbo@gmail.com>
2018-07-14 11:47:34 +0100

    Uncomment shebang tests

--- tests/shebangs.nix
+++ tests/shebangs.nix
@@ -1,35 +1,37 @@
 { helpers, pkgs }:
-helpers.notImplemented "shebangs"/*
 with pkgs;
 with rec {
-  checkShebang = writeScript "check-shebang" ''
-    #!/usr/bin/env bash
-    script="$1"
+  checkShebang = wrap {
+    name   = "check-shebang";
+    script = ''
+      #!/usr/bin/env bash
+      script="$1"
 
-    SHEBANG=$(head -n 1 < "$script")*/
-#    if echo "$SHEBANG" | grep "#![ ]*/bin/sh" > /dev/null
-#    then
-#      echo "#!/bin/sh in $script may break on Debian (dash)" 1>&2
-#      exit 1
-#    fi
-#
-#    if echo "$SHEBANG" | grep "#![ ]*/bin/bash" > /dev/null
-#    then
-#      echo "#!/bin/bash in $script won't work on NixOS" 1>&2
-#      exit 1
-#    fi
-#
-#    if   echo "$SHEBANG" | grep "#![ ]*/usr/bin" > /dev/null &&
-#       ! echo "$SHEBANG" | grep "/usr/bin/env"   > /dev/null
-#    then
-#      echo "Shebang for $script may not work on NixOS"
-#      exit 1
-#    fi
-#
-#    exit 0
-#  '';
-#
-/*  checkScripts = dir: runCommand "check" { inherit checkShebang dir; } ''
+      SHEBANG=$(head -n 1 < "$script")*/
+      if echo "$SHEBANG" | grep "#![ ]*/bin/sh" > /dev/null
+      then
+        echo "#!/bin/sh in $script may break on Debian (dash)" 1>&2
+        exit 1
+      fi
+
+      if echo "$SHEBANG" | grep "#![ ]*/bin/bash" > /dev/null
+      then
+        echo "#!/bin/bash in $script won't work on NixOS" 1>&2
+        exit 1
+      fi
+
+      if   echo "$SHEBANG" | grep "#![ ]*/usr/bin" > /dev/null &&
+         ! echo "$SHEBANG" | grep "/usr/bin/env"   > /dev/null
+      then
+        echo "Shebang for $script may not work on NixOS"
+        exit 1
+      fi
+
+      exit 0
+    '';
+  };
+
+  checkScripts = dir: runCommand "check" { inherit checkShebang dir; } ''
     while read -r script
     do
       "$checkShebang" "$script"
@@ -39,11 +41,12 @@ with rec {
 };
 {
   tests = checkScripts (latestGit {
-    url = "http://chriswarbo.net/git/general-tests.git";
+    url    = "http://chriswarbo.net/git/general-tests.git";
+    stable = { unsafeSkip = true; };
   });
 
   warbo-utilities = checkScripts (latestGit {
-    url = "http://chriswarbo.net/git/warbo-utilities.git";
+    url    = "http://chriswarbo.net/git/warbo-utilities.git";
+    stable = { unsafeSkip = true; };
   });
 }
-*/
