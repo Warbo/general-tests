7c831f3ccaa104fa650816682ff4d76090735e27
Chris Warburton <chriswarbo@gmail.com>
2019-01-11 17:00:26 +0000

    Fix no-backups test

--- tests/no-backups.nix
+++ tests/no-backups.nix
@@ -1,30 +1,39 @@
 { helpers, pkgs }:
-helpers.notImplemented "no-backups"
 
-/*
-#!/usr/bin/env bash
+pkgs.wrap {
+  name   = "no-backups";
+  vars   = { inherit (helpers) HOME; };
+  script = ''
+    #!/usr/bin/env bash
 
-# Pass in the argument "full" to keep going after a failure
-FULL=0
-[[ "x$1" = "xfull" ]] && FULL=1
+    if [[ -z "$FULL" ]]
+    then
+      FULL=0
+      echo "Set env var 'FULL' to keep going after a failure" 1>&2
+    fi
 
-function backups {
-    locate -e "/home/chris/Programming/*~"
-    locate -e "/home/chris/Programming/ * /#*#"
-    locate -e "/.#"
+    function backups {
+      for D in "$HOME"/Programming/*
+      do
+        NAME=$(basename "$D")
+        [[ "x$NAME" = "xgit-html" ]] && continue
+        [[ "x$NAME" = "xNotMine"  ]] && continue
+        echo "Looking for backups in '$D'" 1>&2
+        find "$D" -type f -name '*~' -o -name '#*#' -o -name '.#*'
+      done
+    }
+
+    ERR=0
+    while read -r BACKUP
+    do
+        echo "$BACKUP looks like a backup file" 1>&2
+        ERR=1
+        if [[ "$FULL" -eq 0 ]]
+        then
+          exit 1
+        fi
+    done < <(backups)
+
+    exit "$ERR"
+  '';
 }
-
-function skip {
-    grep -v "/git-html/" | grep -v "/isaplanner-code/"
-}
-
-ERR=0
-while read -r BACKUP
-do
-    echo "$BACKUP looks like a backup file" 1>&2
-    ERR=1
-    [[ "$FULL" -eq 1 ]] || exit 1
-done < <(backups | skip)
-
-exit "$ERR"
-*/
