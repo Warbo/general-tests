e21b5c9b0645be739c38630df4ed7086f07db010
Chris Warburton <chriswarbo@gmail.com>
2019-01-18 07:29:54 +0000

    Update ml4hsfe-test-script

--- tests/ml4hsfe-test-script.nix
+++ tests/ml4hsfe-test-script.nix
@@ -1,9 +1,26 @@
 { helpers, pkgs }:
-helpers.notImplemented "ml4hs-test-script"
-
-/*
-#!/usr/bin/env bash
-set -e
-cd /home/chris/Programming/Haskell/ML4HSFE
-./test.sh
-*/
+with builtins;
+with pkgs;
+with { src = helpers.inputFallback "ml4hs"; };
+wrap {
+  name   = "ml4hs-tests";
+  paths  = (withNix {}).buildInputs ++ [ bash fail ];
+  vars   = withNix {
+    inherit src;
+    names = nixListToBashArray {
+      name = "ATTRNAMES";
+      args = attrNames (import "${src}/overlayed.nix").ml4hsfeTests;
+    };
+  };
+  script = ''
+    #!/usr/bin/env bash
+    set -e
+    for NAME in "$names[@]"
+    do
+      nix-build --no-out-link --show-trace \
+        -E "(import \"$src\").ml4hsfeTests.$NAME" || fail "Couldn't build $NAME"
+    done
+    echo "All passed" 1>&2
+    exit 0
+  '';
+}
