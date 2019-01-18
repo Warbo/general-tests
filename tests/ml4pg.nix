8c8848e372f00592d9c096fc6e732de636c3a256
Chris Warburton <chriswarbo@gmail.com>
2019-01-18 06:51:52 +0000

    Enable ML4PG test

--- tests/ml4pg.nix
+++ tests/ml4pg.nix
@@ -1,5 +1,13 @@
 { pkgs, helpers }:
-helpers.notImplemented "ml4pg"/*
-with { src = helpers.inputFallback "ml4pg"; };
-import "${src}/release.nix"
-*/
+with pkgs;
+wrap {
+  name   = "ml4pg";
+  vars   = withNix { src = helpers.inputFallback "ml4pg"; };
+  paths  = (withNix {}).buildInputs ++ [ bash ];
+  script = ''
+    #!/usr/bin/env bash
+    set -e
+    nix-build --show-trace --no-out-link \
+      -E "(import \"$src/release.nix\").stable.ml4pg"
+  '';
+}
