724ac03c63c1582ab26021e182c451f0aa498792
Chris Warburton <chriswarbo@gmail.com>
2019-01-21 13:52:04 +0000

    Fix panhandle test

--- tests/panhandle.nix
+++ tests/panhandle.nix
@@ -1,39 +1,31 @@
 { helpers, pkgs }:
-helpers.notImplemented "panhandle"/*
 with pkgs;
-runCommand "panhandle" {} ''
-  #!/usr/bin/env bash
-  set -e
+wrap {
+  name   = "panhandle-test";
+  paths  = [ bash fail pandocPkgs ];
+  script = ''
+    #!/usr/bin/env bash
+    set -e
 
-  echo "Checking for panhandle binary"
-  nix-shell --show-trace -p pandocPkgs -p which --run 'which panhandle' || {
-    echo "panhandle binary not installed" 1>&2
-    exit 1
-  }
+    MARKDOWN="*foo*"
+    echo "Got Markdown '$MARKDOWN'"
 
-  MARKDOWN="*foo*"
-  echo "Got Markdown '$MARKDOWN'"
+    JSON=$(echo "$MARKDOWN" | pandoc -f markdown -t json)
+    echo "Got JSON '$JSON'"
 
-  JSON=$(echo "$MARKDOWN" | nix-shell -p pandocPkgs --run "pandoc -f markdown -t json")
-  echo "Got JSON '$JSON'"
+    TICK='`'
+    TICKS="$TICK""$TICK""$TICK"
+    UNWRAP=$(printf '%s{.unwrap}\n%s\n%s' "$TICKS" "$JSON" "$TICKS")
+    echo "Got unwrap '$UNWRAP'"
 
-  TICK='`'
-  TICKS="$TICK""$TICK""$TICK"
-  UNWRAP=$(printf '%s{.unwrap}\n%s\n%s' "$TICKS" "$JSON" "$TICKS")
-  echo "Got unwrap '$UNWRAP'"
+    HTML=$(echo "$UNWRAP" | pandoc --filter panhandle -f markdown -t html) ||
+      fail "Failed to check JSON"
 
-  HTML=$(echo "$UNWRAP" | nix-shell -p pandocPkgs --run \
-           "pandoc --filter panhandle -f markdown -t html") || {
-    echo "Failed to check JSON" 1>&2
-    exit 1
-  }
-  echo "Got HTML '$HTML'"
+    echo "Got HTML '$HTML'"
 
-  echo "$HTML" | grep '<em>foo</em>' || {
-    echo "Didn't unwrap *foo* in '$HTML'" 1>&2
-    exit 1
-  }
+    echo "$HTML" | grep '<em>foo</em>' ||
+      fail "Didn't unwrap *foo* in '$HTML'" 1>&2
 
-  echo "pass" > "$out"
-''
-*/
+    echo "pass"
+  '';
+}
