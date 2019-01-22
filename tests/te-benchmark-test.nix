3d35a6f271cbd918ac8b22041063402f4df2257a
Chris Warburton <chriswarbo@gmail.com>
2019-01-22 19:29:23 +0000

    Remove obsolete test

--- tests/te-benchmark-test.nix
+++
@@ -1,9 +0,0 @@
-{ helpers, pkgs }:
-helpers.notImplemented "te-benchmark-test"/*
-with pkgs;
-with rec {
-  src = helpers.inputFallback "theory-exploration-benchmarks";
-  pkg = import "${src}" {};
-};
-pkg.fullToolTest
-*/
